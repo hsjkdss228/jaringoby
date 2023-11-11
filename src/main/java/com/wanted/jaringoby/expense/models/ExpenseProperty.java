3b783b112cf35a5851ea180c36dc8795ffd86c1a
Inwoo Hwang <hsjkdss228@naver.com>
2023-11-12 03:12:43 +0900

    Feature: Expense Entity 정의

---
+++ src/main/java/com/wanted/jaringoby/expense/models/ExpenseProperty.java
@@ -0,0 +1,18 @@
+package com.wanted.jaringoby.expense.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@EqualsAndHashCode
+public class ExpenseProperty {
+
+    @Column(name = "included_expense_sum")
+    private Boolean includedExpenseSum;
+}
