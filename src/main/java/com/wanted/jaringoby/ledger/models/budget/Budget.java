bf08533c0f6ea6782867739f95a1f8903fe6d0a7
Inwoo Hwang <hsjkdss228@naver.com>
2023-11-12 03:12:20 +0900

    Feature: Budget Entity 정의

---
+++ src/main/java/com/wanted/jaringoby/ledger/models/budget/Budget.java
@@ -0,0 +1,49 @@
+package com.wanted.jaringoby.ledger.models.budget;
+
+import com.wanted.jaringoby.category.models.Category;
+import com.wanted.jaringoby.common.models.Money;
+import com.wanted.jaringoby.common.converters.MoneyConverter;
+import com.wanted.jaringoby.ledger.models.ledger.LedgerId;
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.Column;
+import jakarta.persistence.Convert;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.EmbeddedId;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+@Entity
+@Table(name = "budgets")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Budget {
+
+    @EmbeddedId
+    private BudgetId id;
+
+    @Embedded
+    @AttributeOverride(name = "value", column = @Column(name = "ledger_id"))
+    private LedgerId ledgerId;
+
+    @Column(name = "category")
+    @Enumerated(EnumType.STRING)
+    private Category category;
+
+    @Convert(converter = MoneyConverter.class)
+    @AttributeOverride(name = "value", column = @Column(name = "amount"))
+    private Money amount;
+
+    @Column(updatable = false)
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+}
