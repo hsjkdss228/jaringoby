380b0b8117350cffd6b6adc542aa4979e968d45f
Inwoo Hwang <hsjkdss228@naver.com>
2023-11-21 22:17:04 +0900

    Feature: SecurityFilterChain에 AuthenticationFilter 추가
    - 인증 필터 기본 정의 대신 AuthenticationFilter를 적용
    - WebSecurityCustomizer에 정의된 메서드-경로 쌍을 제외한
      모든 요청이 AuthenticationFilter를 통과

--- src/main/java/com/wanted/jaringoby/common/config/security/SecurityConfig.java
+++ src/main/java/com/wanted/jaringoby/common/config/security/SecurityConfig.java
@@ -1,19 +1,42 @@
 package com.wanted.jaringoby.common.config.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.wanted.jaringoby.common.filters.AuthenticationFilter;
+import com.wanted.jaringoby.common.utils.JwtUtil;
+import jakarta.servlet.Filter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtUtil jwtUtil;
+    private final ObjectMapper objectMapper;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .addFilterBefore(authenticationFilter(), BasicAuthenticationFilter.class);
+
+        return http.build();
+    }
+
+    private Filter authenticationFilter() {
+        return new AuthenticationFilter(jwtUtil, objectMapper);
+    }
 
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
-
         return (web) -> web.ignoring()
                 .requestMatchers(AntPathRequestMatcher.antMatcher(HttpMethod.POST,
                         "/customer/v1.0/customers"))
