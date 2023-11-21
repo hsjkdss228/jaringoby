572a3ea54fee2e28529accd3902cfecf37dba709
Inwoo Hwang <hsjkdss228@naver.com>
2023-11-21 16:31:08 +0900

    Feature: WebSecurityCustomizer Bean 정의
    - 인증/인가를 수행하지 않을 요청 정의
      - 회원가입
      - 로그인
    - 요청을 추가로 정의해야 할 경우, 해당 Bean에 추가

--- src/main/java/com/wanted/jaringoby/common/config/security/SecurityConfig.java
+++ src/main/java/com/wanted/jaringoby/common/config/security/SecurityConfig.java
@@ -2,11 +2,24 @@ package com.wanted.jaringoby.common.config.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 public class SecurityConfig {
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+
+        return (web) -> web.ignoring()
+                .requestMatchers(AntPathRequestMatcher.antMatcher(HttpMethod.POST,
+                        "/customer/v1.0/customers"))
+                .requestMatchers(AntPathRequestMatcher.antMatcher(HttpMethod.POST,
+                        "/customer/v1.0/sessions"));
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
